c3ed2a210d02752e806821e5fea5b3cf3c65bc76
Mateus da Silveira <93267548+MateusSilver@users.noreply.github.com>
2023-03-17 16:19:11 -0300

    Grid & images updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/header';
+import Grid from './components/grid';
+
 import './styles/App.css';
 
 function App() {
   return (
     <div className="container">
       <Header/>
+      <Grid/>
     </div>
   );
 }
---
+++ src/components/grid.js
@@ -0,0 +1,16 @@
+import "../styles/contend.css"
+
+function Grid(){
+    return(
+        <div className="contend">
+			<h1>The House of Literature</h1>
+			<section className="highlights">
+				<div className="principal_highlights"><a href="#"><h3>1618</h3></a></div>
+				<div className="secundary_highlights"><h3>Quadrinhos</h3></div>
+				<div className="secundary_highlights"><a href="Mangas.html"><h3>Mangás</h3></a></div>
+				<div className="secundary_highlights"><h3>Terror</h3></div>
+			</section>
+		</div>
+    )
+}
+export default Grid;
